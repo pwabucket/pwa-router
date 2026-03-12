b2311975027ace07f097d3288b9f15ad63b765e8
Sadiq Salau <sadiqsalau888@gmail.com>
2026-03-12 20:44:20 +0100

    fix: remove unused react plugin and update external dependencies in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import path from "path";
-import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({ tsconfigPath: "./tsconfig.app.json", rollupTypes: true }),
-  ],
+  plugins: [dts({ tsconfigPath: "./tsconfig.app.json", rollupTypes: true })],
   build: {
     lib: {
       name: "@pwabucket/pwa-router",
@@ -17,7 +13,7 @@ export default defineConfig({
       fileName: (format) => (format === "es" ? "index.js" : "index.cjs.js"),
     },
     rollupOptions: {
-      external: ["react", "react-router"],
+      external: ["react", "react-router", "react/jsx-runtime"],
     },
   },
 });
