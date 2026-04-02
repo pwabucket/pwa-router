d06e1be9542334954b184fa024b10ee88bd1214b
Sadiq Salau <sadiqsalau888@gmail.com>
2026-04-02 11:29:29 +0100

    feat: Constants - added global constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const ROUTER_FROM_POSITION = "__router_from_position";
+export const ROUTER_DESTROY_INDEX = "__router_destroy_index";
+export const ROUTER_NAVIGATE_INDEX = "__router_navigate_index";
+export const ROUTER_INDEX_PREFIX = "__router_index_";
